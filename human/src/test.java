3d8d63f6beedc27701ebe4d359e08231f4101d5b
h <h@DESKTOP-QTNDS57>
2022-11-28 10:33:24 +0900

    프로젝트 업로드

---
+++ human/src/test.java
@@ -0,0 +1,26 @@
+
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        byte byteValue = 10;
+        float floatValue = 2.5F;
+        double doubleValue = 2.5;
+
+
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
