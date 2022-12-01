f82a4fa8392a782904ba39fec7848550e856d977
h <h@DESKTOP-QTNDS57>
2022-12-01 19:53:41 +0900

    16

--- human/src/Day15/JDBConnection.java
+++ human/src/Day15/JDBConnection.java
@@ -1,10 +1,6 @@
 package Day15;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBConnection {
 	
@@ -12,30 +8,29 @@ public class JDBConnection {
 	public Statement stmt;
 	public PreparedStatement psmt;
 	public ResultSet rs;
-	
 	// 기본 생성자
 	public JDBConnection() {
-		  
 		try {
 			// JDBC 드라이버 로드
 			// MySQL
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
 			// DB 에 연결
 			// URL:[PORT]/[스키마]?옵션파라미터
 			// MySQL
-			String url = "jdbc:mysql://203.245.44.21:3306/alohasoft?serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true&useSSL=false";
-			String id = "alohasoft";
-			String pw = "shsh0303*";
+			String url = "jdbc:mysql://localhost:3306/human?serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true&useSSL=false";
+			String id = "human";
+			String pw = "123456";
 			
 			con = DriverManager.getConnection(url, id, pw);
 			
 			System.out.println("DB 연결 성공");
 		}
 		catch(Exception e) {
+			System.out.println("DB 연결 실패");
 			e.printStackTrace();
 		}
 	}
+	
 }
 
 
